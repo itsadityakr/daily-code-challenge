dd0258c16da67cb067d70debe5a174e7ff9ab550
Aditya Kumar <itsadityakr27@gmail.com>
2024-12-25 14:11:36 +0530

    day_6 complited

--- Day_6/_28_intersection_and_union_of_sorted_array.cpp
+++ Day_6/_28_intersection_and_union_of_sorted_array.cpp
@@ -1,64 +1,77 @@
-// https://www.naukri.com/code360/problems/intersection-of-2-arrays_1082149
-#include <bits/stdc++.h>
-#define endl "\n"
+// https://www.naukri.com/code360/problems/intersection-of-2-arrays_1082149?leftPanelTabValue=PROBLEM
+#include <iostream>
+#include <vector>
 using namespace std;
 
-vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m) {
-    vector<int> result;
+void findIntersection(vector<int> &A, vector<int> &B) {
+    int n = A.size(), m = B.size();
     int i = 0, j = 0;
+    vector<int> intersection;
     while (i < n && j < m) {
-        if (arr1[i] == arr2[j]) {
-            result.push_back(arr1[i]);
+        if (A[i] == B[j]) {
+            intersection.push_back(A[i]);
             i++;
             j++;
-        } else if (arr1[i] < arr2[j]) {
+        } else if (A[i] < B[j]) {
             i++;
         } else {
             j++;
         }
     }
-    return result;
+    if (!intersection.empty()) {
+        for (int val : intersection) {
+            cout << val << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "-1" << endl;
+    }
 }
 
-vector<int> findArrayUnion(vector<int> &arr1, int n, vector<int> &arr2, int m) {
-    vector<int> result;
+void findUnion(vector<int> &A, vector<int> &B) {
+    int n = A.size(), m = B.size();
     int i = 0, j = 0;
+    vector<int> unionResult;
     while (i < n && j < m) {
-        if (arr1[i] == arr2[j]) {
-            result.push_back(arr1[i]);
+        if (A[i] == B[j]) {
+            unionResult.push_back(A[i]);
             i++;
             j++;
-        } else if (arr1[i] < arr2[j]) {
-            result.push_back(arr1[i]);
+        } else if (A[i] < B[j]) {
+            unionResult.push_back(A[i]);
             i++;
         } else {
-            result.push_back(arr2[j]);
+            unionResult.push_back(B[j]);
             j++;
         }
     }
     while (i < n) {
-        result.push_back(arr1[i]);
+        unionResult.push_back(A[i]);
         i++;
     }
     while (j < m) {
-        result.push_back(arr2[j]);
+        unionResult.push_back(B[j]);
         j++;
     }
-    return result;
+    for (int val : unionResult) {
+        cout << val << " ";
+    }
+    cout << endl;
 }
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> arr1(n), arr2(m);
-    for (int i = 0; i < n; i++) cin >> arr1[i];
-    for (int i = 0; i < m; i++) cin >> arr2[i];
-    vector<int> intersectionResult = findArrayIntersection(arr1, n, arr2, m);
-    if (intersectionResult.empty()) cout << "-1";
-    else for (int i : intersectionResult) cout << i << " ";
-    cout << endl;
-    vector<int> unionResult = findArrayUnion(arr1, n, arr2, m);
-    for (int i : unionResult) cout << i << " ";
-    cout << endl;
+        int n, m;
+        cin >> n >> m;
+
+        vector<int> A(n), B(m);
+        for (int i = 0; i < n; i++) {
+            cin >> A[i];
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> B[i];
+        }
+        findIntersection(A, B);
+        findUnion(A, B);
+
     return 0;
 }
