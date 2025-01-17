1d49b5c970a90b76085c0180512c38247fda886d
Aditya Kumar <itsadityakr27@gmail.com>
2025-01-17 11:06:15 +0530

    updated_day_29

--- Day_29/_102_longest_consecutive_subsequence.cpp
+++ Day_29/_102_longest_consecutive_subsequence.cpp
@@ -1,2 +1,35 @@
-// 
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
 
+class Solution
+{
+public:
+    int longestConsecutive(vector<int> &nums)
+    {
+        if (nums.empty())
+            return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int longest = 1;
+        int currentStreak = 1;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            else if (nums[i] == nums[i - 1] + 1)
+            {
+                currentStreak++;
+            }
+            else
+            {
+                longest = max(longest, currentStreak);
+                currentStreak = 1;
+            }
+        }
+
+        return max(longest, currentStreak);
+    }
+};
