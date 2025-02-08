23f9e71805401a5f3be60875c451e7ee729ee3ac
Aditya Kumar <itsadityakr27@gmail.com>
2025-02-08 14:45:46 +0530

    updated_code

--- daily-problems-day-50/_196_Reverse_Words_in_a_String.cpp
+++ daily-problems-day-50/_196_Reverse_Words_in_a_String.cpp
@@ -1,27 +1,78 @@
 // https://leetcode.com/problems/reverse-words-in-a-string/
 
-class Solution {
+class Solution
+{
 public:
-    string reverseWords(string s) {
+    string reverseWords(string s)
+    {
         vector<string> words;
         stringstream ss(s);
         string word;
-        
-        while (ss >> word) {
+
+        while (ss >> word)
+        {
             words.push_back(word);
         }
-        
+
         reverse(words.begin(), words.end());
-        return join(words, " ");
-    }
-    
-private:
-    string join(const vector<string>& words, const string& delimiter) {
+
         string result;
-        for (const string& word : words) {
-            if (!result.empty()) result += delimiter;
-            result += word;
+        for (int i = 0; i < words.size(); i++)
+        {
+            if (i > 0)
+                result += " ";
+            result += words[i];
         }
+
         return result;
     }
 };
+
+// class Solution
+// {
+// public:
+//     string reverseWords(string s)
+//     {
+//         removeExtraSpaces(s);
+//         reverse(s.begin(), s.end());
+//         reverseEachWord(s);
+//         return s;
+//     }
+
+// private:
+//     void removeExtraSpaces(string &s)
+//     {
+//         int n = s.size(), i = 0, j = 0;
+//         while (i < n && s[i] == ' ')
+//             i++;
+//         while (i < n)
+//         {
+//             if (s[i] != ' ')
+//             {
+//                 s[j++] = s[i++];
+//             }
+//             else if (j > 0 && s[j - 1] != ' ')
+//             {
+//                 s[j++] = ' ';
+//                 while (i < n && s[i] == ' ')
+//                     i++;
+//             }
+//         }
+//         if (j > 0 && s[j - 1] == ' ')
+//             j--;
+//         s.resize(j);
+//     }
+
+//     void reverseEachWord(string &s)
+//     {
+//         int start = 0, end = 0, n = s.size();
+//         while (start < n)
+//         {
+//             while (end < n && s[end] != ' ')
+//                 end++;
+//             reverse(s.begin() + start, s.begin() + end);
+//             start = end + 1;
+//             end++;
+//         }
+//     }
+// };
